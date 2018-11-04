5ada0785f38a6c60d718ed87643f4c8440d6e1f3
Sharad Ghimire <sharadghimire5551@gmail.com>
2018-11-04 21:39:59 +1100

    Get routes for profile with id and handle added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,6 +40,40 @@ router.get(
   }
 );
 
+// @route   GET api/profile/handle/:handle
+// @desc    Get Profile by handle
+// @access  Public
+router.get('/handle/:handle', (req, res) => {
+  let errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'profileimg'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user.';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/profile/user/:id
+// @desc    Get Profile by user ID
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+  let errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'profileimg'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user.';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route   POST api/profile
 // @desc    Create or Edit users profile
 // @access  Protected
